fcab2de77140c02f8f7824644cab3b71861e6929
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2018-09-07 10:58:20 +0200

    software and part entities

---
+++ src/Entity/Software.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SoftwareRepository")
+ */
+class Software
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $version;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $detail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
+
+    public function setVersion(string $version): self
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    public function setDetail(?string $detail): self
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+}
