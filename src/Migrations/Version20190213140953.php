a38b7876f13990008ccddeecbb2d6d91dd712c57
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2019-02-13 16:01:14 +0100

    supression de la créaation de la table mouvement dans les migrations

--- src/Migrations/Version20190213140953.php
+++ src/Migrations/Version20190213140953.php
@@ -15,7 +15,6 @@ final class Version20190213140953 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE mouvement');
         $this->addSql('ALTER TABLE brand CHANGE category_id category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE category CHANGE image_id image_id INT DEFAULT NULL, CHANGE hours_per_day hours_per_day INT DEFAULT NULL, CHANGE mtbf mtbf INT DEFAULT NULL');
         $this->addSql('ALTER TABLE equipment CHANGE brand_id brand_id INT DEFAULT NULL, CHANGE contract_id contract_id INT DEFAULT NULL');
@@ -42,7 +41,6 @@ final class Version20190213140953 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE mouvement (MVIDMouvement INT NOT NULL, MVDate DATE DEFAULT \'NULL\', MVDateEnregistrement DATETIME DEFAULT \'NULL\', MVIDUser TINYINT(1) NOT NULL, MVEtat VARCHAR(2) DEFAULT \'NULL\' COLLATE utf8_general_ci, MVCommentaire TEXT DEFAULT NULL COLLATE utf8_general_ci, MVCommentaire2 TEXT DEFAULT NULL COLLATE utf8_general_ci, MVCommentClient TEXT DEFAULT NULL COLLATE utf8_general_ci, MVFichier_BL VARCHAR(512) DEFAULT \'NULL\' COLLATE utf8_general_ci, MVVersionLogicielle VARCHAR(10) DEFAULT \'NULL\' COLLATE utf8_general_ci, MVVersionBuild INT NOT NULL, MVNumSerie VARCHAR(30) DEFAULT \'NULL\' COLLATE utf8_general_ci, MVIDSite VARCHAR(10) DEFAULT \'NULL\' COLLATE utf8_general_ci, MVCodeDescriptionEtat TINYINT(1) DEFAULT \'NULL\', MVCodeDescriptionEtat2 TINYINT(1) DEFAULT \'NULL\', MVNoPanne TINYINT(1) DEFAULT \'0\' NOT NULL, MVCodeDiagnostic TINYINT(1) DEFAULT \'NULL\', MVCodeDiagnostic2 TINYINT(1) DEFAULT \'NULL\', MVSoft TINYINT(1) DEFAULT \'0\' NOT NULL, MVStats TINYINT(1) DEFAULT \'0\' NOT NULL, MVDegradation TINYINT(1) DEFAULT \'0\' NOT NULL, MVRepaSiteOise TINYINT(1) DEFAULT \'0\' NOT NULL, MVRepaVix TINYINT(1) DEFAULT \'0\' NOT NULL, MVParam TINYINT(1) DEFAULT \'0\' NOT NULL, MVTempsRepa INT DEFAULT NULL, IdEquipment INT NOT NULL, state TINYINT(1) NOT NULL, INDEX WDIDX_MOUVEMENT_MVIDSite (MVIDSite), INDEX WDIDX_MOUVEMENT_MVCodeDescriptionEtat (MVCodeDescriptionEtat), INDEX WDIDX_MOUVEMENT_MVDate (MVDate), INDEX WDIDX_MOUVEMENT_MVEtat (MVEtat), INDEX WDIDX_MOUVEMENT_MVNumSerie (MVNumSerie), PRIMARY KEY(MVIDMouvement)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE brand CHANGE category_id category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE category CHANGE image_id image_id INT DEFAULT NULL, CHANGE hours_per_day hours_per_day INT DEFAULT NULL, CHANGE mtbf mtbf INT DEFAULT NULL');
         $this->addSql('ALTER TABLE equipment CHANGE brand_id brand_id INT DEFAULT NULL, CHANGE contract_id contract_id INT DEFAULT NULL');
