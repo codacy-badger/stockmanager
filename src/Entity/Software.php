9ac1f0f4a9b9b773359152e5aa6094e1215e7ba4
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2019-04-29 15:33:44 +0200

    hotfix supression de la contrainte d'unicité sur le champs version

--- src/Entity/Software.php
+++ src/Entity/Software.php
@@ -7,7 +7,6 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\SoftwareRepository")
- * @UniqueEntity(fields={"version"}, message="Cette version existe déjà.")
  */
 class Software
 {
--- src/Form/RepairType.php
+++ src/Form/RepairType.php
@@ -14,7 +14,6 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
